2e08006597604a03ef931c2417f703d946d1213f
Paul Kinsky <pkinsky@gmail.com>
2014-03-17 21:53:57 -0400

    test descriptions
    based on redid case study. refactor, stripping away all functions not
    mentioned in case study. Re-add later.

--- test/UserOpsSpec.scala
+++ test/UserOpsSpec.scala
@@ -12,6 +12,74 @@ need to ensure this is run against test redis instance, eventually. Until then m
 
 object StringSpecification extends PlaySpecification {
 
+  /*
+
+  features from case study
+
+  1. set up a user with a unique id and related data
+     (GET username:username:uid => if isDefined, error
+     INCR global:nextUserId => unique, no sequential guarantee (don't test for order, only uniqueness)
+     SET uid:1000:username antirez
+     SET uid:1000:password p1pp0
+     SET username:antirez:uid 1000
+
+  test: register N users with name and password equal to prefix + iteration #, check that users can be retrieved with same info
+
+  functions
+    register_user(username, user_id) => incr uid, use below setter functions
+    get/set user_id(username)
+    get/set username(user id)
+    get/set password
+
+
+
+  2. follower/following, posts (of interest to user) (this section only describes data structure, not actions)
+    uid:1000:followers => Set of uids of all the followers users
+    uid:1000:following => Set of uids of all the following users
+
+    test:
+      follow, check following list for follower, followee
+
+    functions:
+      follow_user(follower, following)
+      get following
+      get followers
+
+  make a post
+      uid:1000:posts => a List of post ids - every new post is LPUSHed here.
+      INCR global:nextPostId => 10343
+      SET post:10343 "$owner_id|$time|I'm having fun with Retwis
+
+  distribute a post to recipients
+      foreach($followers as $fid) {
+        $r->push("uid:$fid:posts",$postid,false);
+
+  fetch posts
+    LRANGE uid:1000:posts
+
+  test: have N users follow user a and not user b. users a and b post M messages. check that all messages availible from uid:1000:posts
+
+  function:
+    make post (user, post body)
+    fetch user posts(user) //single range for now, add pagination later
+
+
+
+  3.authentication (test getter/setter, I suppose (also, setter will encapsulate generating a new auth string)
+    SET uid:1000:auth fea5e81ac8ca77622bed1c2132a021f9
+    SET auth:fea5e81ac8ca77622bed1c2132a021f9 1000
+
+  test: generate and fetch some auth tokens
+
+  functions
+    gen_auth_token(user id)
+    get user(token)
+    get token(user id)
+
+
+
+   */
+
   val redis_service = RedisServiceImpl
 
   "user ops" should {
